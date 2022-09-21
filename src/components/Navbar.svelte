ef350f83f1ba8e632ab23776d4072bd0475d8354
Michael Festus <micfestus45@gmail.com>
2022-09-21 03:08:31 +0100

    Fixed preloader to aid SEO, shortened loading time

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,6 +1,6 @@
 <script>
 </script>
 
-<div class="flex items-center justify-center w-full max-w-[1224px] mx-auto py-6 lg:items-left lg:mx-5 lg:justify-start">
+<div class="flex items-center justify-center w-full max-w-[1224px] mx-auto py-6 lg:items-left lg:mx-auto lg:justify-start">
 <img src="/assets/Uruggo.svg" alt="Uruggo's logo">
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,27 +4,18 @@
 	import Footer from '../components/Footer.svelte';
 	import { onMount } from 'svelte';
 
-	let loading: boolean = true;
 	let loader: any;
 	let time = 0;
-	let timer = 600;
+	let timer = 50;
 
 	function addOne() {
 		time += 1;
-		if (time >= 20) {
-			timer = timer / 2;
-		} else if (time >= 40) {
-			timer = timer / 2;
-		} else if (time >= 60) {
-			timer = timer / 2;
-		}
 		changeTimer(timer);
 	}
 	function changeTimer(timer: number) {
 		let timeout = setTimeout(addOne, timer);
 		if (time === 100) {
 			clearTimeout(timeout);
-			loading = false;
 		}
 	}
 	onMount(() => {
@@ -36,59 +27,61 @@
 	});
 </script>
 
-{#if loading}
+<!-- Preloader -->
+<div class="relative flex justify-center w-full h-full">
 	<div
-		class="loader bg-black w-screen h-screen relative left-0 top-0 flex items-center justify-center"
+		class="loader z-10 bg-black w-screen h-screen relative left-0 top-0 flex items-center justify-center"
 		bind:this={loader}
 	>
 		<p class="text-[#9BA809] text-6xl text-center absolute">{time}%</p>
-		<img src="/assets/Uruggo.svg" class="logo opacity-25" alt="Uruggo's Logo">
+		<img src="/assets/Uruggo.svg" class="logo opacity-25" alt="Uruggo's Logo" />
 	</div>
-{:else}
-	<div class="lg:mx-5 content">
+	<!-- Preloader -->
+
+	<div class="lg:mx-5 content absolute">
 		<Navbar />
 		<slot />
 		<Footer />
 	</div>
-{/if}
+</div>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=DM+Sans:wght@500&display=swap');
 
-	.content{
+	.loader {
 		animation-name: load;
 		animation-duration: 1s;
+		animation-delay: 3s;
 		animation-timing-function: ease-in-out;
 		animation-fill-mode: forwards;
 	}
-	.logo{
+	.logo {
 		animation-name: sizeImage;
 		animation-duration: 2s;
-		animation-timing-function: cubic-bezier(.25,.46,.45,.94);
+		animation-timing-function: cubic-bezier(0.25, 0.46, 0.45, 0.94);
 		animation-iteration-count: infinite;
 		animation-fill-mode: backwards;
 	}
-	p{
+	p {
 		font-family: 'DM Sans', sans-serif;
 	}
 
-
-	@keyframes load{
+	@keyframes load {
 		from {
-			transform: translateY(-100%);
+			transform: translateY(0%);
 		}
 		to {
-			transform: translateX(0%)
+			transform: translateY(-100%);
 		}
 	}
-	@keyframes sizeImage{
-		0%{
+	@keyframes sizeImage {
+		0% {
 			scale: 1;
 		}
-		50% { 
-			scale: 1.5
+		50% {
+			scale: 1.5;
 		}
-		100%{
+		100% {
 			scale: 1;
 		}
 	}
